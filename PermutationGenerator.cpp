508700d58d6c608896e8bbf0f0afe72c74f42223
lixiaohan <lixh14@mails.tsinghua.edu.cn>
2018-11-03 15:53:19 +0800

    Init API

---
+++ PermutationGenerator.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 李晓涵 on 2018/11/3.
+//
+#include "PermutationGenerator.h"
+#include <iostream>
+using namespace std;
+
+void PermutationGenerator::generate_permutations(int method, string optput_file){
+  switch (method){
+    case PermutationGenerator::RECURSION:
+      cout << "recursion" << endl;
+  }
+}
+
+void PermutationGenerator::recursion(string output_file){
+
+}
+
+void PermutationGenerator::classic_lexi(string output_file){
+
+}
+
+void PermutationGenerator::SJT_method(string output_file){
+
+}
+
+void PermutationGenerator::mediator_lexi(string output_file){
+
+}
+
+void PermutationGenerator::inc_carrying(string output_file){
+
+}
+
+void PermutationGenerator::dec_carrying(string output_file){
+
+}
+
+void PermutationGenerator::swapping(string output_file){
+
+}
